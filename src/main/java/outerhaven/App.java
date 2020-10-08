1a534382c305adc1bec90df24fc4bd8b152b65ef
achalhii <ilyes.achalhi@etu.umontpellier.fr>
2020-10-08 17:58:21 +0200

    test

--- src/main/java/outerhaven/App.java
+++ src/main/java/outerhaven/App.java
@@ -1,13 +1,22 @@
 package outerhaven;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class App  {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("test");
+        FlowPane root = new FlowPane();
+        Scene test = new Scene(root,1258,750, Color.color(0,0,0));
+        primaryStage.setScene(test);
+        primaryStage.show();
     }
 }
