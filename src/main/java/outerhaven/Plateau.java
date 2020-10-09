dd048e7445ceec9ce0da5b394e330073755532ff
achalhii <ilyes.achalhi@etu.umontpellier.fr>
2020-10-09 22:00:40 +0200

    test

--- src/main/java/outerhaven/Plateau.java
+++ src/main/java/outerhaven/Plateau.java
@@ -1,9 +1,38 @@
 package outerhaven;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javax.swing.text.html.ImageView;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Plateau {
-    ArrayList<Case> aire;
+    int aire;
     ArrayList<Personne> personnages;
     ArrayList<Personne> morts;
+    private static Stage primary;
+
+
+
+    public Plateau(int aire ,Stage primary) {
+        this.aire = aire;
+        this.primary=primary;
+    }
+    public void lancerScenePlateu(){
+        ImageView hexagone_img = new ImageView(new Image(Plateau.class.getResourceAsStream("./Image/Hexagone.png")));
+        hexagone_img.setSize(1,2);
+
+        HBox hexagone = new HBox();
+
+        Group group = new Group();
+        //group.getChildren().add(hexagone);
+
+        Scene scene = new Scene(group,1280 , 720, Color.WHITE);
+        primary.setScene(scene);
+        primary.show();
+
+    }
 }
