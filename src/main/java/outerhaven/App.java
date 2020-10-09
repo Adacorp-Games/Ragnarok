dffacdd797e7d17019319904c7c3ace9baef6bdf
Hamza <hamza.ikiou@etu.umontpellier.fr>
2020-10-09 20:59:33 +0200

    classe archer

--- src/main/java/outerhaven/App.java
+++ src/main/java/outerhaven/App.java
@@ -1,28 +1,58 @@
 package outerhaven;
 
-/*import javafx.application.Application;
+import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
 public class App extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("test");
-        FlowPane root = new FlowPane();
-        Scene test = new Scene(root,1258,750, Color.color(0,0,0));
-        primaryStage.setScene(test);
-        primaryStage.show();
+    public void start(Stage stage) {
+
+        // Create MenuBar
+        MenuBar menuBar = new MenuBar();
+
+        // Create menus
+        Menu fileMenu = new Menu("Fichier");
+        Menu editMenu = new Menu("Edit");
+        Menu helpMenu = new Menu("Aide");
+
+        // Create MenuItems
+        MenuItem newItem = new MenuItem("Nouveau");
+        MenuItem openFileItem = new MenuItem("Ouvrir Fichier");
+        MenuItem exitItem = new MenuItem("Quitter");
+
+        MenuItem copyItem = new MenuItem("Copier");
+        MenuItem pasteItem = new MenuItem("Coller");
+
+        // Add menuItems to the Menus
+        fileMenu.getItems().addAll(newItem, openFileItem, exitItem);
+        editMenu.getItems().addAll(copyItem, pasteItem);
+
+        // Add Menus to the MenuBar
+        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
+
+        BorderPane root = new BorderPane();
+        root.setTop(menuBar);
+        Scene scene = new Scene(root, 350, 200);
+
+        stage.setTitle("Menu");
+        stage.setScene(scene);
+        stage.show();
     }
-}*/
-public class App {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
+/*public class App {
     public static void main(String[] args) {
         System.out.println("test");
     }
-}
+}*/
 
---
+++ src/main/java/outerhaven/Archer.java
@@ -0,0 +1,7 @@
+package outerhaven;
+
+public class Archer extends Personne{
+    public Archer() {
+        super(Personne.getRandomName(), 150, 50, 150, 100, 2, 1);
+    }
+}
