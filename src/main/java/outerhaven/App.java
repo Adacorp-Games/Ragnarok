7a73cb35836a17f61c00f86fc07f2588f07730f7
achalhii <ilyes.achalhi@etu.umontpellier.fr>
2020-10-10 12:47:35 +0200

    plateau fait

--- src/main/java/outerhaven/App.java
+++ src/main/java/outerhaven/App.java
@@ -1,11 +1,6 @@
 package outerhaven;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -13,7 +8,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
     stage.setTitle("test");
-    Plateau test = new Plateau(5,stage);
+    stage.setFullScreen(true);
+    Plateau test = new Plateau(100,stage);
     test.lancerScenePlateu();
     }
     public static void main(String[] args) {
