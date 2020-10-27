b7093504b6a3af0cb2277542b9787f7a97082364
Pierre-Antoine Chéron <pierre-antoine.cheron@etu.umontpellier.fr>
2020-10-27 11:56:44 +0100

    Path to perso V1

--- src/main/java/outerhaven/Case.java
+++ src/main/java/outerhaven/Case.java
@@ -74,11 +74,10 @@ public class Case {
     }
 
     public boolean estOccupe() {
-        if (contenu.size()==0) {
+        if (contenu.size() == 0) {
             return false;
-        }
-        else {
-            if (contenu.size()==1) {
+        } else {
+            if (contenu.size() == 1) {
                 return true;
             }
             System.out.println("Attention la case contient plus d'un obstacle/unite");
@@ -106,22 +105,22 @@ public class Case {
         return caseVoisines;
     }
 
-    public void setCaseVoisines(ArrayList<Case> voisines){
+    public void setCaseVoisines(ArrayList<Case> voisines) {
         this.caseVoisines = voisines;
     }
 
-    public void addVoisin(Case v){
+    public void addVoisin(Case v) {
         caseVoisines.add(v);
     }
 
-    public void setContenu(Personne p){
-        if(estOccupe()){
+    public void setContenu(Personne p) {
+        if (estOccupe()) {
             System.out.println("la case contient deja un obstacle/unite");
             contenu.add(p);
         }
     }
 
-    public ArrayList<Personne> getContenu(){
+    public ArrayList<Personne> getContenu() {
         return contenu;
     }
 
@@ -129,24 +128,32 @@ public class Case {
         this.couleur = couleur;
     }
 
-    public ArrayList<Case> voisinsLibres(){
+    public ArrayList<Case> voisinsLibres(boolean lib) {
         ArrayList<Case> libres = new ArrayList<>();
         for (Case case1 : caseVoisines) {
-            if( !case1.estOccupe()){
-                libres.add(case1);
+            if (lib) {
+                if (!case1.estOccupe()) {
+                    libres.add(case1);
+                }
+            } else {
+                if (case1.estOccupe()) {
+                    libres.add(case1);
+                }
             }
+
         }
         return libres;
     }
 
+    @Deprecated
     public ArrayList<Case> pathToPerso(Personne p, ArrayList<Case> parcours) {
         parcours.add(this);
-        if(contenu.contains(p)){
+        if (contenu.contains(p)) {
             return parcours;
         }
         ArrayList<Case> aParcourir = new ArrayList<Case>();
-        for (Case case1 : voisinsLibres()) {
-            if(!parcours.contains(case1)){
+        for (Case case1 : voisinsLibres(true)) {
+            if (!parcours.contains(case1)) {
                 aParcourir.add(case1);
             }
         }
@@ -156,14 +163,44 @@ public class Case {
         }
         ArrayList<Case> shorterPath = new ArrayList<Case>();
         for (ArrayList<Case> path : cheminsEnfants) {
-            if (shorterPath.size()==0) {
+            if (shorterPath.size() == 0) {
                 shorterPath = path;
             } else {
-                if (path.size()<shorterPath.size()) {
+                if (path.size() < shorterPath.size()) {
                     shorterPath = path;
                 }
             }
         }
         return shorterPath;
     }
+
+    public ArrayList<Case> pathToPersoAux(Equipe equipe, ArrayList<Case> parcours, int depth) {
+        if (depth == 0) {
+            if (contenu.get(0).getTeam() == equipe) {
+                parcours.add(this);
+                return parcours;
+            }
+        }
+        ArrayList<ArrayList<Case>> parcoursEnfants = new ArrayList<ArrayList<Case>>();
+        parcours.add(this);
+        for (Case voisin : voisinsLibres(depth != 1)) {
+            ArrayList<Case> parcoursVoisin = voisin.pathToPersoAux(equipe, parcours, -1);
+            if (parcoursVoisin.size() == depth) {
+                parcoursEnfants.add(parcoursVoisin);
+            }
+        }
+        return parcoursEnfants.get(0);
+    }
+
+    // necessite que le plateau contienne au moins un personnage de l'equipe visee en dehors de this
+    public ArrayList<Case> pathToPerso(Equipe e) {
+        ArrayList<Case> parcours = new ArrayList<Case>();
+        int depth = 1;
+        while (parcours.isEmpty()) {
+            parcours = pathToPersoAux(e, new ArrayList<Case>(), depth);
+            depth++;
+        }
+        return parcours;
+    }
+
 }
