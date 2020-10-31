2434ac5aa7d26c50012539bc824c6c0b543d745c
achalhii <ilyes.achalhi@etu.umontpellier.fr>
2020-10-31 15:44:01 +0100

    reparation bouton menu

--- src/main/java/outerhaven/Plateau.java
+++ src/main/java/outerhaven/Plateau.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
@@ -14,6 +13,7 @@ import outerhaven.Interface.BarrePersonnage;
 import outerhaven.Interface.Effets;
 import outerhaven.Personnages.Personne;
 
+
 import java.util.ArrayList;
 
 public class Plateau {
@@ -21,25 +21,20 @@ public class Plateau {
     public static ArrayList<Personne> personnages = new ArrayList<>();
     public static ArrayList<Personne> morts = new ArrayList<>();
     public static ArrayList<Case> listeCase = new ArrayList<>();
-    public static ArrayList<Equipe> listeEquipe = new ArrayList<>();
     private static Stage primary;
     public static Double taille;
     public static Personne personneSelectionné;
     public static Equipe equipeSelectionné;
     public static Group group = new Group();
     public static Scene scene = new Scene(group);;
-    double largeurMax = Screen.getPrimary().getVisualBounds().getHeight();
+        double largeurMax = Screen.getPrimary().getVisualBounds().getHeight();
     double longeurMax = Screen.getPrimary().getVisualBounds().getWidth();
-    public static boolean statusPartie = false;
-    public static BarrePersonnage barre = new BarrePersonnage();
-    public Equipe e1 = new Equipe(Color.RED);
-    public Equipe e2 = new Equipe(Color.BLUE);
 
     public Plateau(Stage primary) {
         Plateau.primary = primary;
     }
     public void lancerPartie() {
-        interfaceDebut();
+        interfacedebut();
         primary.setScene(scene);
         primary.show();
     }
@@ -77,13 +72,15 @@ public class Plateau {
             }
         }
 
-        // Creation et incorporation d'une slide barre + boutton
+        //creation et incorporation d'une slide barre + boutton
+        BarrePersonnage barre = new BarrePersonnage();
         ajouteLeMenu();
-        group.getChildren().add(boutonEquipe());
+        equipe();
+
         group.getChildren().add(barre.returnBarre());
         primary.setScene(scene);
     }
-    private void interfaceDebut() {
+    private void interfacedebut(){
         Button start = new Button("START");
         start.setLayoutX((longeurMax-700)/2);
         start.setLayoutY((largeurMax-200)/2);
@@ -112,18 +109,6 @@ public class Plateau {
                 lancerScenePlateau();
             }
         });
-        nbCase.setOnKeyReleased(key -> {
-            if (key.getCode() == KeyCode.ENTER) {
-                aire = getIntFromTextField(nbCase);
-                if (aire > 0) {
-                    group.getChildren().clear();
-                    lancerScenePlateau();
-                }
-            }
-        });
-
-        listeEquipe.add(e1);
-        listeEquipe.add(e2);
         group.getChildren().add(infoNB);
         group.getChildren().add(nbCase);
         group.getChildren().add(start);
@@ -134,19 +119,21 @@ public class Plateau {
         return Integer.parseInt(text);
     }
 
-    public void incorporeEquipe(Equipe e1) {
+    public void incorporeEquipe(Equipe e1){
         equipeSelectionné = e1;
     }
 
-    private Group boutonEquipe() {
+    private void equipe(){
         Button equipe1 = new Button("Equipe 1");
         equipe1.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
+        Equipe e1 = new Equipe(Color.RED);
         equipe1.setLayoutX(10);
         equipe1.setLayoutY(800);
         equipe1.setMinSize(60, 20);
 
         Button equipe2 = new Button("Equipe 2");
         equipe2.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
+        Equipe e2 = new Equipe(Color.BLUE);
         equipe2.setLayoutX(80);
         equipe2.setLayoutY(800);
         equipe2.setMinSize(60, 20);
@@ -162,28 +149,25 @@ public class Plateau {
             equipe2.setEffect(new Effets().putInnerShadow(e2.getCouleur()));
             equipe1.setEffect(null);
         });
-
-        Group groupEquipeButton = new Group();
-        groupEquipeButton.getChildren().add(equipe1);
-        groupEquipeButton.getChildren().add(equipe2);
-        return groupEquipeButton;
+        group.getChildren().add(equipe1);
+        group.getChildren().add(equipe2);
     }
 
-    private Button boutonExit() {
+    private Button boutonexit(){
         Button exit = new Button("Quitter");
         exit.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
         exit.setLayoutX(10);
-        exit.setLayoutY(125);
+        exit.setLayoutY(90);
         exit.setMinSize(60,20);
         exit.setOnMouseClicked(mouseEvent -> primary.close());
         return exit;
     }
 
-    private Button boutonReset() {
-        Button reset = new Button("Nouvelle grille");
+    private Button boutonreset(){
+        Button reset = new Button("Nouvelle Grille");
         reset.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
         reset.setLayoutX(10);
-        reset.setLayoutY(90);
+        reset.setLayoutY(125);
         reset.setMinSize(60,20);
         reset.setOnMouseClicked(mouseEvent -> {
             group.getChildren().clear();
@@ -192,7 +176,7 @@ public class Plateau {
         return reset;
     }
 
-    private Button boutonReStart() {
+    private Button boutonreStrat(){
         Button reStrat = new Button("Restart");
         reStrat.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
         reStrat.setLayoutX(10);
@@ -205,13 +189,13 @@ public class Plateau {
         return reStrat;
     }
 
-    private void boutonPausePlay() {
+    private void boutonPausePlay(){
         Label labelPlay = new Label("");
         labelPlay.setLayoutY(670);
         Label labelPause = new Label("");
         labelPause.setLayoutY(650);
         Button pause = new Button("Pause");
-        Button play = new Button("Jouer");
+                Button play = new Button("Jouer");
         play.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
         play.setLayoutX(75);
         play.setLayoutY(20);
@@ -220,16 +204,7 @@ public class Plateau {
             labelPlay.setText("La partie reprend");
             group.getChildren().remove(play);
             group.getChildren().add(pause);
-            System.out.println(personnages.size());
-            setStatusPartie(true);
-            if (group.getChildren().contains(barre.returnBarre())) {
-                group.getChildren().remove(barre.returnBarre());
-                //group.getChildren().remove(boutonEquipe());
-                scene.setFill(Color.DARKGRAY);
-            }
-            tour();
         });
-
         pause.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
         pause.setLayoutX(75);
         pause.setLayoutY(20);
@@ -238,38 +213,35 @@ public class Plateau {
             labelPause.setText("La partie est en pause");
             group.getChildren().remove(pause);
             group.getChildren().add(play);
-            setStatusPartie(false);
-            if (!group.getChildren().contains(barre.returnBarre())) {
-                group.getChildren().add(barre.returnBarre());
-                //group.getChildren().add(boutonEquipe());
-                scene.setFill(Color.WHITE);
-            }
         });
         group.getChildren().add(play);
         group.getChildren().add(labelPause);
         group.getChildren().add(labelPlay);
     }
 
-    private void ajouteLeMenu() {
+    private void ajouteLeMenu(){
         Button menu = new Button("Menu");
         menu.setStyle("-fx-background-color: lightgrey;-fx-border-style: solid;-fx-border-width: 2px;-fx-border-color: black");
         menu.setLayoutX(10);
         menu.setLayoutY(20);
         menu.setMinSize(60,20);
+        Button exit = boutonexit();
+        Button reSTart = boutonreStrat();
+        Button reset = boutonreset();
         menu.setOnMouseClicked(mouseEvent -> {
-            if (!group.getChildren().contains(boutonExit())) {
+            if (!group.getChildren().contains(exit)) {
                 try {
-                    group.getChildren().add(boutonReset());
-                    group.getChildren().add(boutonReStart());
-                    group.getChildren().add(boutonExit());
+                    group.getChildren().add(reset);
+                    group.getChildren().add(reSTart);
+                    group.getChildren().add(exit);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             } else {
                 try {
-                    group.getChildren().remove(boutonReset());
-                    group.getChildren().remove(boutonReStart());
-                    group.getChildren().remove(boutonExit());
+                    group.getChildren().remove(reset);
+                    group.getChildren().remove(reSTart);
+                    group.getChildren().remove(exit);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -277,22 +249,5 @@ public class Plateau {
         });
         boutonPausePlay();
         group.getChildren().add(menu);
-    }
-
-    public void tour() {
-        while (e1.getTeam().size() != 0 || e2.getTeam().size() != 0 || statusPartie != false) {
-            for (Personne p : personnages) {
-                p.action();
-            }
-        }
-    }
-
-    // Getter et setter
-    public static boolean getStatusPartie() {
-        return statusPartie;
-    }
-
-    public static void setStatusPartie(boolean statusPartie) {
-        Plateau.statusPartie = statusPartie;
     }
 }
