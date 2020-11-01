dd01eb3e81ba883faa5378168cc85963302f7f2b
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2020-11-01 01:54:47 +0100

    Test ajout icone application

--- src/main/java/outerhaven/App.java
+++ src/main/java/outerhaven/App.java
@@ -1,6 +1,7 @@
 package outerhaven;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -9,6 +10,7 @@ public class App extends Application {
     public void start(Stage stage) {
         stage.setTitle("Ragnarok");
         stage.setFullScreen(true);
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("./Images/Ragnarok.png")));
         Plateau game = new Plateau(stage);
         game.lancerPartie();
     }
