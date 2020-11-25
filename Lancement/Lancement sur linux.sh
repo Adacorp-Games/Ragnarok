8de8bf50704186a3e313a5cc98c4160cae8dadc6
Pierre-Antoine Chéron <pierre-antoine.cheron@etu.umontpellier.fr>
2020-11-25 13:42:19 +0100

    nettoyage dossier lancement

--- Lancement/Lancement sur linux.sh
+++
@@ -1,3 +0,0 @@
-export PATH_TO_FX=../ressources/JDK/javafx-sdk-15/lib
-javac --module-path $PATH_TO_FX --add-modules javafx.controls ../src/main/java/outerhaven/$(find . -name "*.java")
-java --module-path $PATH_TO_FX --add-modules javafx.controls App
